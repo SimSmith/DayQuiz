f21a991bcc05c59556241cd5d63cd62318fab313
Smith <greatsimonsmith@gmail.com>
2019-09-29 18:44:25 +0200

    Increase the span of random years
    And add toggleable cheat sheet.

--- index.js
+++ index.js
@@ -6,27 +6,33 @@ qElement.innerText = question_str(date)
 
 
 function question_str(d) {
-    var options = { year: 'numeric', month: 'long', day: 'numeric' };
-    return d.toLocaleString('en-GB', options);
+    var options = { year: 'numeric', month: 'long', day: 'numeric' }
+    return d.toLocaleString('en-GB', options)
 }
 
 function randomDate() {
-    var startDay = new Date(2019, 1, 1);
-    var endDay = new Date(2019, 11, 31);
+    var startDay = new Date(1890, 1, 1)
+    var endDay = new Date(2200, 11, 31)
     var randTime = (endDay.getTime() - startDay.getTime()) * Math.random() + startDay.getTime()
-    var date = new Date(randTime);
-    return date;
+    var date = new Date(randTime)
+    return date
 }
 
 function onSelectAnwser(index){
     console.log("index", index)
     var response = document.getElementById("text")
     if (index == date.getDay()) {
-        response.innerText = "Correct, anwser was " + date.toDateString();
+        response.innerText = "Correct, anwser was " + date.toDateString()
     } else {
-        response.innerText = "Wrong, anwser was " + date.toDateString();
+        response.innerText = "Wrong, anwser was " + date.toDateString()
     }
     console.log("Input comming")
     date = randomDate()
     qElement.innerText = question_str(date)
-};
+}
+
+function toggleImageVisible(id) {
+    var img = document.getElementById(id)
+    vis = img.style.visibility
+    img.style.visibility = vis == 'visible' ? 'hidden' : 'visible' // default: hidden
+}
